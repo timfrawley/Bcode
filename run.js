98bb07cbadc969d0fb19758347392f34510cefbe
Tim Frawley <timfrawley1998@gmail.com>
2017-02-02 13:17:41 -0500

    added the run.js

---
+++ run.js
@@ -0,0 +1,14 @@
+var Quagga = require('quagga').default;
+Quagga.decodeSingle({
+    src: "test.jpg",
+    numOfWorkers: 0,  // Needs to be 0 when used within node 
+    decoder: {
+        readers: ["upc_reader"] // List of active readers 
+    },
+}, function(result) {
+    if(result.codeResult) {
+        console.log("result", result.codeResult.code);
+    } else {
+        console.log("not detected");
+    }
+});
